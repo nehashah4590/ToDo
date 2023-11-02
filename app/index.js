94ec1cb6afe350b8a6164cc624050f8d74fc9b1b
nehashah4590 <ns457989@gmail.com>
2023-11-02 18:54:51 +0545

    added task component

--- app/index.js
+++ app/index.js
@@ -1,11 +1,40 @@
-import {View, Text} from "react-native";
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+import Task from "./components/Task.js";
 
 function index() {
   return (
-  
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+
+        {/* Todays Tasks */}
+        <View style={styles.tasksWrapper}>
+            <Text style={styles.sectionTitles}>Today's tasks</Text>
+            <View style={styles.items}>
+            <Task task={"task1"}/>
+            <Task task={"task2"}/>
+            </View>
+        </View>
+    </View>
    
   )
 }
 
 export default index
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#E8EAED',
+    },
+    tasksWrapper: {
+        paddingTop: 80,
+        paddingHorizontal: 20,
+    },
+    sectionTitles: {
+        fontSize:24,
+        fontWeight: "bold"
+    },
+    items: {
+        
+    },
+})
